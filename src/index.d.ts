4ee391ae5b6e9754661b317b442395d40c234c23
Sahil Sinha <sahil.sinha@juspay.in>
2022-05-02 15:43:10 +0530

    Updated type for callback handler

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,15 @@
 import { InitiatePayload } from "./models/InitiatePayload";
 import { ProcessPayload } from "./models/ProcessPayload";
 
+type JSONObject = {
+  [key: string]: any
+};
+
 export class HyperServices {
   static preFetch(preFetchPayload: any): void;
   initiate(
     initiatePayload: InitiatePayload,
-    callback: (event: string) => void
+    callback: (event: JSONObject) => void
   ): void;
   process(processPayload: ProcessPayload): void;
   terminate(): void;
