776c0f3582882cf7125624ad6d70b2530fc86dba
diveshjoshi-juspay <138568830+diveshjoshi-juspay@users.noreply.github.com>
2023-07-04 14:22:30 +0530

    Add support for UpiIntent and EC in Services
    In order to use UpiIntent and EC as services add UpiIntent and EC in Services

--- src/models/Service.ts
+++ src/models/Service.ts
@@ -1,1 +1,1 @@
-export type Service = "in.juspay.hyperpay" | "in.juspay.hyperapi" | "in.juspay.flyer";
+export type Service = "in.juspay.hyperpay" | "in.juspay.hyperapi" | "in.juspay.flyer" | "in.juspay.ec" | "in.juspay.upiintent";
